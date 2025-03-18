d238c7b67528f3e79d3f21487e568c6b446491b4
stevenzm22 <stevenjosez03@gmail.com>
2025-03-18 14:13:14 -0600

    terminacion de la pagina principal y footer

--- src/componentes/footer/Footer.jsx
+++ src/componentes/footer/Footer.jsx
@@ -8,21 +8,27 @@ function Footer() {
 
   return (
     <div id='footer'>
-       <div>
+
+       <div className='div'>
         <h2>Contactos</h2>
-      
+        <i className="bi bi-telephone-fill"></i> 6190-6284
+        <i className="bi bi-telephone-fill"></i> 6190-9393
        </div>
 
-       <div>
-        <h2>Horario</h2>
+       <div className='div'>
+        <h2> Horario</h2>
+        <h5><i className="bi bi-calendar-date"></i> lunes a viernes </h5>
+        <p>6am a 12 pm</p>
        </div>
 
-       <div>
-        <h2>Direcion</h2>
+       <div className='div'>
+        <h2>dirección</h2>
+        <i className="bi bi-geo-alt"></i> <a href="https://maps.app.goo.gl/mqGLVr3BkqRWsR857" target="_blank">primera entra de San luis,costado oeste de la escuela,145 mts norte</a>
        </div>
 
-       <div>
-        <h2>Redes</h2>
+       <div className='div'>
+        <h2>Redes Sociales</h2>
+        <i class="bi bi-whatsapp"></i> 6199-2244
        </div>
 
     </div>
---
+++ src/componentes/homeAdmin/HomeAdmin.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import "../homeAdmin/Style.css"
+
+function HomeAdmin() {
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default HomeAdmin
